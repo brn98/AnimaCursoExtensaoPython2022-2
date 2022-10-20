cee2d26ca77a8bb5fdd8228ebcda97c784491b5d
Bruno Costa <brn.98@live.com>
2022-10-20 00:52:25 +0000

    Adicionado variável nome

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 #
 # print() = comando de saida
 print("Olá mundo!")
+
+
+# Quando quiser guardar uma String!
+nome = "Bruno Costa"
+idade = 24
+
+#Exibir meu nome atribuido a variável nome!
+print("Meu nome é " + nome)
