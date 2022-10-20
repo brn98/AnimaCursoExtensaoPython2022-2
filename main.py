11fe391469f058cb6fed6f497c4ec42cd8221262
Bruno Costa <brn.98@live.com>
2022-10-20 01:17:32 +0000

    Aprendendo a usar print e concatenar

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ nome = "Bruno Costa"
 idade = 24
 
 #Exibir meu nome atribuido a variável nome!
-print("Meu nome é " + nome)
+print("Meu nome é", nome)
+
+#Exibir idade atribuida a variavel idade!
+print("Tenho", idade, "anos!")
+print(f"Tenho {idade} anos!\n")
+
+#Quando quiser exibir "Meu nomé ... e tenho .. anos..." trocando pelas variáveis!
+print("Meu nome é {} e tenho {} anos!".format(nome, idade))
+
