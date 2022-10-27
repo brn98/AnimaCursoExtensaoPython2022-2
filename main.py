7538cfd5d0a920771bc3f8dca67eb2822a882ff8
Bruno Costa <brn.98@live.com>
2022-10-27 01:24:22 +0000

    Spoiler de laço de repetição

---
+++ main.py
@@ -0,0 +1,21 @@
+#Exemplo de laço
+
+#Se eu quiser exibir números de 1 a 10?
+print("Aqui mostramos de maneira rústica")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10\n")
+
+print("Aqui mostramos do jeito certo")
+numero = 1
+while numero <= 10: 
+  print(numero)
+  numero = numero + 1 
+
