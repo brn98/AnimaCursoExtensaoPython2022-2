8b352a2819b28d326021b5d95ad54aa22867dd9d
Bruno Costa <brn.98@live.com>
2022-10-27 00:28:34 +0000

    Estruturas condicionais

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 #Comando input(): quero permitir que 
 #O usuário digite algo...
 
-nome = input("Digite seu nome: ")
+nome = input("Qual o seu nome? ")
 idade = int(input("Agora informe sua idade: "))
+gen = input("E o seu gênero M ou F?")
 
 #Comando de saída...
 
-print("Seu nome é {} e você tem {} anos!".format(nome,idade))
+print("Seu nome é {} e você tem {} anos!\n".format(nome,idade))
 
 #Mostrando o dobro da idade e ano de nascimento
 ano = 2022
 dobro = idade*2
 nascimento = ano-idade
-print("O dobro da sua idade é {}, seu ano de nascimento é {}".format(dobro,nascimento))
+print("O dobro da sua idade é {}, seu ano de nascimento é {}!\n".format(dobro,nascimento))
+
+#Estruturas condicionais
+#E se eu quiser perguntar o gênero (M = Masculino e F = Feminino)
+#Mostre (... E você também precisa/precisou prestar o serviço militar obrigatório)
+
+if (idade>=18): 
+  print("Vocês é maior de idade, ótimo! Já pode beber ou dirigir.")
+  if (gen == 'M'): print("E você também precisa/precisou prestar o serviço militar obrigatório!")
+else: 
+  menorIdade=18-idade
+  print(f"Vocês é menor de idade, que pena! Falta {menorIdade} anos pra você poder beber ou dirigir\n")
+
+
